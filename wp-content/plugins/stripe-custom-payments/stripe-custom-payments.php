3d46d27c8f9746517aac86e737524428da4c7d88
Vladimir Pivnichenko <pivnichenko.amgrade@gmail.com>
2018-05-24 16:53:53 +0300

    start create plugin

---
+++ wp-content/plugins/stripe-custom-payments/stripe-custom-payments.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Stripe custom payments
+ * Plugin URI:        https://github.com/web-user/custom-shop-wordpress
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Volodymyr
+ * Author URI:        https://github.com/web-user/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       stripe-custom-payments
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
